9e456004affc0b318d44cd61e23e0670908ed413
Yuri Palacio <yurifpalacio@gmail.com>
2024-08-12 21:00:04 -0300

    Adding tests for edit lesson with attachments

--- src/domain/event/enterprise/entities/lesson-attachment.ts
+++ src/domain/event/enterprise/entities/lesson-attachment.ts
@@ -1,7 +1,7 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/unique-entity-id'
 
-interface LessonAttachmentProps {
+export interface LessonAttachmentProps {
   lessonId: UniqueEntityId
   attachmentId: UniqueEntityId
 }
---
+++ test/repositories/in-memory-lesson-attachments-repository.ts
@@ -0,0 +1,16 @@
+import { LessonAttachmentsRepository } from '@/domain/event/application/repositories/lesson-attachments-repository'
+import { LessonAttachment } from '@/domain/event/enterprise/entities/lesson-attachment'
+
+export class InMemoryLessonAttachmentsRepository
+  implements LessonAttachmentsRepository
+{
+  public items: LessonAttachment[] = []
+
+  async findManyByLessonId(lessonId: string): Promise<LessonAttachment[]> {
+    const lessonAttachments = this.items.filter(
+      (item) => item.lessonId.toString() === lessonId,
+    )
+
+    return lessonAttachments
+  }
+}
